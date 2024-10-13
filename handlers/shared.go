d325f1e14e39df3e2106a9ce9c5f240ca36f0f34
Gustybobby <napattatiyakaroonwong@gmail.com>
2024-10-14 01:58:29 +0700

    feat: add handlers helper for route with repo

--- handlers/shared.go
+++ handlers/shared.go
@@ -1,15 +1,26 @@
 package handlers
 
 import (
+	"football-stat-goth/repos"
 	"log/slog"
 	"net/http"
 )
 
 type HTTPHandler func(w http.ResponseWriter, r *http.Request) error
 
+type HTTPRepoHandler func(w http.ResponseWriter, r *http.Request, repo *repos.Repository) error
+
 func Make(h HTTPHandler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if err := h(w, r); err != nil {
+			slog.Error("Handler error", "err", err)
+		}
+	}
+}
+
+func MakeWithRepo(h HTTPRepoHandler, repo *repos.Repository) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := h(w, r, repo); err != nil {
 			slog.Error("Handler error", "err", err)
 		}
 	}
