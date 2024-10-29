2f1ce38a1ec18b92bdd21186f6edb07855d887fa
Gustybobby <napattatiyakaroonwong@gmail.com>
2024-10-29 20:26:10 +0700

    fix: filter and order fixture by date time

--- repos/match.go
+++ repos/match.go
@@ -1,10 +1,18 @@
 package repos
 
-import "football-stat-goth/models"
+import (
+	"football-stat-goth/models"
+	"time"
+)
 
 func FindFixtureMatches(repo *Repository) ([]models.Match, error) {
 	var matches []models.Match
-	results := repo.DB.Model(&models.Match{}).Preload("HomeLineup.Club").Preload("HomeLineup").Preload("AwayLineup.Club").Preload("AwayLineup").Find(&matches)
+	var earliestTimeString string = time.Now().Add(time.Duration(-3) * time.Hour).UTC().Format(time.RFC3339)
+	results := repo.DB.Model(&models.Match{}).Preload(
+		"HomeLineup.Club").Preload(
+		"AwayLineup.Club").Where(
+		"date_time >= ?", earliestTimeString).Order(
+		"date_time ASC").Find(&matches)
 	if results.Error != nil {
 		return nil, results.Error
 	}
