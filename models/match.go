8aac165cabd0cd876e0dcffe6720676d4fcffb5c
Gustybobby <napattatiyakaroonwong@gmail.com>
2024-10-29 20:36:34 +0700

    feat: add attr default vals

--- models/match.go
+++ models/match.go
@@ -11,10 +11,11 @@ type Match struct {
 	HomeLineupID uint      `gorm:"uniqueIndex" json:"home_lineup_id"`
 	AwayLineupID uint      `gorm:"uniqueIndex" json:"away_lineup_id"`
 	Season       string    `gorm:"not null" json:"season"`
-	HomeGoals    uint      `gorm:"not null" json:"home_goals"`
-	AwayGoals    uint      `gorm:"not null" json:"away_goals"`
+	HomeGoals    uint      `gorm:"not null;default:0" json:"home_goals"`
+	AwayGoals    uint      `gorm:"not null;default:0" json:"away_goals"`
 	Location     string    `gorm:"not null" json:"location"`
 	DateTime     time.Time `gorm:"not null" json:"date_time"`
+	MatchWeek    uint      `gorm:"not null" json:"match_week"`
 
 	HomeLineup Lineup `gorm:"foreignKey:HomeLineupID;references:LineupID"`
 	AwayLineup Lineup `gorm:"foreignKey:AwayLineupID;references:LineupID"`
