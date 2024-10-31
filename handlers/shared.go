36128c6a72a1055feba4fd8109f2793961abd1b2
Gustybobby <napattatiyakaroonwong@gmail.com>
2024-10-31 17:56:12 +0700

    chore: remove api key auth

--- handlers/shared.go
+++ handlers/shared.go
@@ -1,14 +1,11 @@
 package handlers
 
 import (
-	"errors"
 	"football-stat-goth/repos"
 	"log/slog"
 	"net/http"
-	"os"
 
 	"github.com/a-h/templ"
-	"github.com/joho/godotenv"
 )
 
 type HTTPHandler func(w http.ResponseWriter, r *http.Request, repo *repos.Repository) error
@@ -23,14 +20,4 @@ func Make(h HTTPHandler, repo *repos.Repository) http.HandlerFunc {
 
 func Render(w http.ResponseWriter, r *http.Request, c templ.Component) error {
 	return c.Render(r.Context(), w)
-}
-
-func AuthAPIKey(r *http.Request) error {
-	if err := godotenv.Load(); err != nil {
-		return err
-	}
-	if r.Header.Get("apikey") != os.Getenv("API_KEY") {
-		return errors.New("invalid api key")
-	}
-	return nil
 }
