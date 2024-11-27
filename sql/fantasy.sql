d3ff644345720a0483a8243477fefb0bd25383c6
TayR-D <tayra.devacc@gmail.com>
2024-11-27 14:59:40 +0700

    feat: added blank-card for fantasy minor update on filter

--- sql/fantasy.sql
+++ sql/fantasy.sql
@@ -1,13 +1,6 @@
--- name: GetPlayerInfoForFantasy :many
-SELECT "player".lastname, "player".position, "player".image, "club".id as club_id
-FROM "player" 
-inner join "club_player" on "player".id = "club_player".player_id
-inner join "club" on "club_player".club_id = "club".id
-ORDER BY "player".position ASC, "player".lastname ASC;
-
 -- name: GetFantasy_PlayerInfoForFantasy :many
 SELECT "player".lastname, "player".position, "player".image, "club".id as club_id, "fantasy_player".cost, "fantasy_player".points, "fantasy_player".rating
 FROM "fantasy_player" 
-inner join "player" on "fantasy_player".player_id = "player".id
-inner join "club" on "fantasy_player".club_id = "club".id
+JOIN "player" on "fantasy_player".player_id = "player".id
+JOIN "club" on "fantasy_player".club_id = "club".id
 ORDER BY "player".position ASC, "player".lastname ASC;
