22898aa015a1a165f8215d62bec54389ef784156
Gustybobby <napattatiyakaroonwong@gmail.com>
2024-11-29 00:54:05 +0700

    feat: add players pagination

--- handlers/cmps/players.go
+++ handlers/cmps/players.go
@@ -6,17 +6,24 @@ import (
 	"football-stat-goth/repos"
 	"football-stat-goth/views/components/table_components"
 	"net/http"
+	"strconv"
 )
 
 func HandlePlayersTable(w http.ResponseWriter, r *http.Request, repo *repos.Repository) error {
+	page, err := strconv.Atoi(r.URL.Query().Get("page"))
+	if err != nil || page < 1 {
+		page = 1
+	}
+	page_size := 30
+
 	players, err := repo.Queries.ListPlayerLikeFullname(repo.Ctx, queries.ListPlayerLikeFullnameParams{
 		FullnameLike: "%" + r.FormValue("fullname") + "%",
-		Offset:       0,
-		Limit:        30,
+		Offset:       int32((page - 1) * page_size),
+		Limit:        int32(page_size),
 	})
 	if err != nil {
 		return err
 	}
 
-	return handlers.Render(w, r, table_components.PlayersTable(players))
+	return handlers.Render(w, r, table_components.PlayersTable(players, page))
 }
--- handlers/pages/players.go
+++ handlers/pages/players.go
@@ -6,10 +6,16 @@ import (
 	"football-stat-goth/services/plauth"
 	"football-stat-goth/views"
 	"net/http"
+	"strconv"
 )
 
 func HandlePlayersPage(w http.ResponseWriter, r *http.Request, repo *repos.Repository) error {
 	user := plauth.GetContextUser(r)
 
-	return handlers.Render(w, r, views.Players(user))
+	page, err := strconv.Atoi(r.URL.Query().Get("page"))
+	if err != nil || page < 1 {
+		page = 1
+	}
+
+	return handlers.Render(w, r, views.Players(user, page))
 }
